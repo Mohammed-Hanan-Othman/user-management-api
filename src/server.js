44ac4b951b083c23985b003cad89e79a746b5511
mohammed-hanan-othman <mohananothman@gmail.com>
2025-03-18 22:25:40 +0000

    Added logging and rate limiting.

--- src/server.js
+++ src/server.js
@@ -1,18 +1,47 @@
 require("dotenv").config();
 const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+const TEN_MINUTES = 10 * 60 * 1000;
+
 const userRouter = require("./routes/authRouter");
 
 const app = express();
 
+// Security headers
+app.use(helmet());
+
+// Enable CORS
+app.use(cors());
+
+// Logging
+app.use(morgan("dev"));
+
+// Rate limiting
+const limiter = rateLimit({
+    windowMs: TEN_MINUTES,
+    max: 30,
+    message: "Too many requests, please try again later."
+});
+app.use(limiter);
+
+// Request parsers
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// Initial default route
+//Routes
 app.use("/api/v1/users", userRouter);
 app.get("/", (req, res) => {
     res.status(200).send("User Management API is running");
 });
 
+// Error Handler
+app.use((err, req, res, next)=>{
+    console.log(err);
+    res.status(500).json({ message: "Internal server error"});
+})
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
     console.log(`SERVER is running on port ${PORT}`);
