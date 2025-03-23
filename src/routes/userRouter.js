2a885fc6d0cafc7282db8e17c50a73fbd641411f
mohammed-hanan-othman <mohananothman@gmail.com>
2025-03-23 16:01:19 +0000

    Added user creation validation logic

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,29 @@
 const express = require("express");
+const { check, validationResult } = require("express-validator");
 const { getUsers, createUser } = require("../controllers/userController");
 
+const validatePostUser = [
+    // name, email, password
+    check("name").notEmpty().trim().withMessage("Name cannot be empty"),
+    check("email").notEmpty().trim().withMessage("Email cannot be empty")
+        .isEmail().withMessage("Email is invalid"),
+    check("password").notEmpty().trim().withMessage("Password cannot be empty")
+        .isLength({min: 6}).withMessage("Password must be at least 6 characters long")
+];
+const handleValidationErrors = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: errors.array()
+        });
+    }
+    next();
+}
 const userRouter = express.Router();
 
 userRouter.get("/", getUsers);
-userRouter.post("/",createUser);
+userRouter.post("/",validatePostUser,handleValidationErrors,createUser);
 
 
 module.exports = userRouter;
