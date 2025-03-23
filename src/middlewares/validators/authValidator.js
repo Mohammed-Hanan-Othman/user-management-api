1e8d356af9e166b0f59aa9e00cc7f9cee30d4e22
mohammed-hanan-othman <mohananothman@gmail.com>
2025-03-23 17:29:51 +0000

    Updated validators

--- src/middlewares/validators/authValidator.js
+++ src/middlewares/validators/authValidator.js
@@ -7,6 +7,15 @@ const validateLogin = [
         .isLength({min: 6}).withMessage("Password must be at least 6 characters long")
 ];
 
+const validateSignup = [
+    check("name").notEmpty().trim().withMessage("Name cannot be empty"),
+    check("email").notEmpty().trim().withMessage("Email cannot be empty")
+        .isEmail().withMessage("Email is invalid"),
+    check("password").notEmpty().trim().withMessage("Password cannot be empty")
+        .isLength({min: 6}).withMessage("Password must be at least 6 characters long")
+];
+
 module.exports = {
-    validateLogin
+    validateLogin,
+    validateSignup
 };
