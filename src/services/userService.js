5d35e0cc565d1bf4e541f4a57b2d30491855d09a
mohammed-hanan-othman <mohananothman@gmail.com>
2025-03-25 22:11:59 +0000

    Updated user service methods

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,7 +1,7 @@
 const prisma = require("../config/prisma");
 
 const getAllUsers = async (filters, options) =>{
-    const {page, limit, orderBy} = options;
+    const { page, limit, orderBy } = options;
     return await prisma.user.findMany({
         where: filters,
         take : limit,
@@ -10,24 +10,36 @@ const getAllUsers = async (filters, options) =>{
         omit: { password: true }
     });
 }
+const getUserById = async (id, hidePassword = true) =>{
+    return await prisma.user.findUnique({ 
+        where: { id },
+        omit: hidePassword ? { password: true} : undefined
+    });
+}
 const getUserByEmail = async (email, hidePassword = true) => {
-    const options = {
+    return await prisma.user.findUnique({
         where: { email },
         omit: hidePassword ? { password: true} : undefined
-    };
-
-    return await prisma.user.findUnique(options);
-};
+    });
+}
 const getUserCount = async (filters) =>{
     return await prisma.user.count({ where : filters });
 }
-
 const createNewUser = async (data) => {
-    return await prisma.user.create({ data, omit: {password: true} });
+    return await prisma.user.create({ data, omit: { password: true }});
+}
+const updateExistingUser = async (data, id, hidePassword = true) =>{
+    return await prisma.user.update({
+        data, 
+        where: { id },
+        omit: hidePassword? { password: true } : undefined
+    });
 }
 module.exports = {
     getAllUsers,
     getUserCount,
     getUserByEmail,
-    createNewUser
+    getUserById,
+    createNewUser,
+    updateExistingUser
 }
