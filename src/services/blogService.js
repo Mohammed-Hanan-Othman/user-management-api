e2312493d2ae99047c4c988d3866ee76859450ae
mohammed-hanan-othman <mohananothman@gmail.com>
2025-04-08 13:20:34 +0000

    Added blog service methods

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -1,5 +1,10 @@
 const prisma = require("../config/prisma");
 
+const getBlogsCount = async (filters = {}) =>{
+    return await prisma.blog.count({ 
+        where : filters 
+    });
+};
 const createBlog = async (data) => {
     return await prisma.blog.create({
         data, 
@@ -8,7 +13,18 @@ const createBlog = async (data) => {
         }
     });
 };
+const getAllBlogs = async (filters, options) => {
+    const { page, limit, orderBy } = options
+    return await prisma.blog.findMany({
+        where: filters,
+        take: limit,
+        skip: (page - 1) * limit,
+        orderBy,
+    });
+};
 
 module.exports = {
+    getBlogsCount,
     createBlog,
+    getAllBlogs,
 };
