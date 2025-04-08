a92bf63e7fa2cf8e776e30f5b035dc8f8c0ff43e
mohammed-hanan-othman <mohananothman@gmail.com>
2025-04-08 13:21:11 +0000

    Implemented blog fetching with pagination, filtering and sorting

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,4 +1,4 @@
-const { createBlog } = require("../services/blogService");
+const { createBlog, getAllBlogs, getBlogsCount } = require("../services/blogService");
 const { getUserById } = require("../services/userService");
 
 const postBlog = async (req, res) => {
@@ -25,7 +25,66 @@ const postBlog = async (req, res) => {
         return res.status(500).json({ message: "Internal Server Error" });   
     }
 };
+const getBlogs = async (req, res) =>{
+    try {
+        let { page = 1, limit = 10, title, content, sortBy = "createdAt", order = "desc", search=" " } = req.query;
+        page = parseInt(page);
+        limit = parseInt(limit);
+        if (page < 1 || limit < 1) {
+            return res.status(400).json({ 
+                message: "Page and limit must be positive values"
+            });
+        }
+        // Set filters
+        let filters = search ? {
+            OR: [
+                    { title : { contains: search, mode: "insensitive"} },
+                    { content : { contains: search, mode: "insensitive"} }
+            ]
+        }:{};
+        if (title) filters["title"] = { contains: title };
+        if (content) filters["content"] = { contains: content };
+        if (filters.OR.length === 0) delete filters.OR;
 
+        // Validate order
+        const validOrder = ["asc", "desc"];
+        if (!validOrder.includes(order.toLowerCase())) {
+            return res.status(400).json({ 
+                message: "Invalid order value. Use 'asc' or 'desc'." 
+            });
+        }
+        // Create sort order
+        let orderBy = [];
+        if (Array.isArray(sortBy)){
+            orderBy = sortBy.map(item => ({ [item]: order.toLowerCase() }));
+        } else {
+            orderBy = [{ [sortBy] : order.toLowerCase() }]
+        }
+
+        const options = { page, limit, orderBy }
+        // Get all blogs
+        const blogs = await getAllBlogs(filters, options)
+
+        // Generate metadata
+        const totalBlogs = await getBlogsCount(filters);
+        const totalPages = Math.ceil(totalBlogs / limit);
+        const metadata = {
+            total_records : totalBlogs,
+            total_pages: totalPages,
+            current_page: page,
+            limit,
+        };
+        return res.status(200).json({
+            message: "Fetched blogs successfully",
+            metadata,
+            data: blogs
+        });
+    } catch (error) {
+        console.error("Error fetching blogs:", error.message || error);
+        return res.status(500).json({ message: "Internal Server Error" });
+    }
+}
 module.exports = {
-    postBlog
+    postBlog,
+    getBlogs
 }
--- src/routes/blogRouter.js
+++ src/routes/blogRouter.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const { protectRoute } = require("../middlewares/protectRoute");
-const { validatePostBlog } = require("../middlewares/validators/blogValidator");
+const { validatePostBlog, validateBlogId } = require("../middlewares/validators/blogValidator");
 const { handleValidationErrors } = require("../middlewares/handleValidationErrors");
-const { postBlog } = require("../controllers/blogController");
+const { postBlog, getBlogs } = require("../controllers/blogController");
 
 const blogRouter = express.Router();
 
+blogRouter.get("/", getBlogs);
 blogRouter.post("/",protectRoute, validatePostBlog, handleValidationErrors, postBlog);
-
 module.exports = blogRouter;
